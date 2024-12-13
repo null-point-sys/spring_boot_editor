1fca9970a832976f57fa671b272413587c82510a
alejoZapata <jaalza@gmail.com>
2024-12-13 00:17:01 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -2,6 +2,6 @@
 
 public class UserCode {
     public static void main(String[] args) {
-        System.out.println("Hello, asasdasdasdasd!");
+        System.out.println("Hello, github actions!");
     }
 }
