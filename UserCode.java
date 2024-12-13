df7fdfdc1c25cbeca5e2d2269f9ac5a58e0f18ef
alejoZapata <jaalza@gmail.com>
2024-12-13 00:41:00 -0500

    Update UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -1,7 +1,12 @@
-// Escribe tu código Java aquí
+package com.example.demo;
 
 public class UserCode {
     public static void main(String[] args) {
         System.out.println("Hello, fff!");
+    }
+
+    // Método a simular en el test
+    public String someMethod() {
+        return "Actual Output";
     }
 }
