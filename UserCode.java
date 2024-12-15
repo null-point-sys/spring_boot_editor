d82585b915060d0850de9792f93839d248503c29
alejoZapata <jaalza@gmail.com>
2024-12-15 00:38:56 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -8,5 +8,8 @@
           }
       
           // Implementación inicial o placeholder (puede estar vacío o lanzar una excepción)
-
+          @Override
+          public String someMethod() {
+              throw new UnsupportedOperationException("Debes implementar this method.");
+          }
       }
