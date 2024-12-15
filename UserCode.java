286f1aff91be7ab82a78c000a0356c8e2fb59864
alejoZapata <jaalza@gmail.com>
2024-12-14 19:22:36 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -7,6 +7,7 @@
             System.out.println("Hello, fff!");
         }
       
+      
         // Método a simular en el test
          public String someMethod() {
             return "Actual Output";
