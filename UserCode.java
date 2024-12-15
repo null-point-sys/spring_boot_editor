ec300c2804cd6e5601258c15dd010604de9265f2
alejoZapata <jaalza@gmail.com>
2024-12-14 22:59:38 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -1,12 +1,6 @@
-// Escribe tu código Java aquí
-      package com.example.demo;
-      
-      public class UserCode implements UserCodeInterface {
-      
-          public static void main(String[] args) {
-              System.out.println("Hello, fff!");
-          }
-      
-          // Implementación inicial o placeholder (puede estar vacío o lanzar una excepción)
-          
-      }
+package com.example.demo;
+
+public class UserCode {
+    public static void main(String[] args) {
+        System.out.println("Hello, fff!");
+    }
