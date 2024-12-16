744aa9746136f93d3694aa278d9a9183368768a2
alejoZapata <jaalza@gmail.com>
2024-12-15 22:28:58 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -5,5 +5,7 @@ package com.example.demo;
           public static void main(String[] args) {
               System.out.println("Hello, fff!");
           }
-        
+      
+          // Implementación inicial o placeholder (puede estar vacío o lanzar una excepción)
+          
       }
