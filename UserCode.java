dbe78e462a326d2ccce670f7f6406f03ef521a82
alejoZapata <jaalza@gmail.com>
2024-12-20 00:38:47 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -5,7 +5,8 @@ package com.example.demo;
           public static void main(String[] args) {
               System.out.println("Hello, fff!");
           }
-        
+      
+          // Implementación inicial o placeholder (puede estar vacío o lanzar una excepción)
           public String someMethod() {
               throw new UnsupportedOperationException("Debes implementar this method.");
           }
