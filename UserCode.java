a83f3828198af7da4ae0009e02c3a2fc224481b9
alejoZapata <jaalza@gmail.com>
2024-12-23 11:48:20 -0500

    Guardar c�digo de usuario en UserCode.java

--- UserCode.java
+++ UserCode.java
@@ -7,4 +7,8 @@ package com.example.demo;
         }
     
       
+      
+        public String someMethod() {
+            throw new UnsupportedOperationException("Debes implementar this method.");
+        }
     };
