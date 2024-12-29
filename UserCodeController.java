33fa599196a566d338aa04177aed4f7a1df61b34
alejoZapata <jaalza@gmail.com>
2024-12-29 00:15:03 -0500

    Guardar c�digo de usuario en UserCodeController.java

--- UserCodeController.java
+++ UserCodeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
   
+import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
