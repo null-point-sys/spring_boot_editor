0fcf212522dda923612962815098f56c629805cd
alejoZapata <jaalza@gmail.com>
2024-12-29 23:19:23 -0500

    Guardar c�digo de usuario en UserCodeModel.java

--- UserCodeModel.java
+++ UserCodeModel.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-// 777
 @Entity
 public class User {
     private Long id;  // Usar Long en lugar de long para permitir valores nulos
